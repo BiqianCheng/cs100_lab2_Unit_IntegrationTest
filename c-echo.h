d14779a0f6b69b8315e03e9187d877541fa954ac
KatelynCheng <chengbiqian@foxmail.com>
2020-01-21 00:17:12 -0800

    writing a unit test

--- c-echo.h
+++ c-echo.h
@@ -1,14 +1,12 @@
 #include <iostream>
-using namespace std;
 
-string echo(int length, char** chars){
-  string ret = "";
-  for(int i=1; i< length; i++){
-     ret += char[i]};
-     if(i < length -1){
-        ret += " ";
-     }
-  }  
-  ret += "\n";
-  return ret;
+std::string echo(int length, char** chars) {
+    std::string ret = "";
+    for(int i = 1; i < length; i++) {
+        ret += chars[i];
+        if(i < length - 1) {
+            ret += " ";
+        }
+    }
+    return ret;
 }
