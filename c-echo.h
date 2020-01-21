0c4d7715badead89ba7ce0b79f36c564db0f1fba
KatelynCheng <chengbiqian@foxmail.com>
2020-01-20 22:51:24 -0800

    Cmake and unit testing

---
+++ c-echo.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+string echo(int length, char** chars){
+  string ret = "";
+  for(int i=1; i< length; i++){
+     ret += char[i]};
+     if(i < length -1){
+        ret += " ";
+     }
+  }  
+  ret += "\n";
+  return ret;
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "c-echo.h"
+using namespace std;
+
+int main(int argv, char** argc){
+  cout << echo(argv, argc);
+}
